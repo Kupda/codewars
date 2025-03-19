4a7b1b48cf0ba5c51fbe93fb11388ab15fb45360
Daniil Kupriyanov <103760207+Kupda@users.noreply.github.com>
2025-03-19 09:00:08 +0300

    Update Explosive Sum.py

--- Explosive Sum.py
+++ Explosive Sum.py
@@ -1,61 +1,38 @@
-# https://www.codewars.com/kata/52ec24228a515e620b0005ef
-
-# def exp_sum(num):
-#     terms = []
-#     flag = True
-#     c = 1
-#     while flag:
-#         if num > 1:
-#             terms = [1, num-1]
-#             c += 1
-#             print(terms)
-#             while flag:
-#                 if terms[-1] > 1:
-#                     last_num = terms[-1]
-#                     terms.pop(-1)
-#                     terms.append(1)
-#                     terms.append(last_num-1)
-#                     c += 1
-#                     print(terms)
-#                 else:
-#                     flag = False
-#         else:
-#             flag = False
-#     return c
-#
-# number = 5
-# print([number])
-# print(exp_sum(number))
-
-
 def exp_sum(num):
     diagram = [num]  # [4]
     c = 1
+    print([10], c)
     flag = True
     while flag:
         last_num = diagram[0]  # 4
         if last_num != 1:
             diagram.pop(0)
-            diagram.append(last_num - 1)
+            diagram.insert(0, last_num - 1)
             diagram.append(1)
             c += 1
-            print(diagram)
-            for index in range(0, len(diagram)):
-                if diagram[index] > 1:
-                    diagram[index] -= 1
-                    try:
-                        diagram[index+1] += 1
-                    except:
-                        diagram.append(1)
-                    print(diagram)
-                    c += 1
-                    diagram = sorted(diagram, reverse=True)
-                    print(diagram, "сортировка")
-
+            print(diagram, c)
+            flag1 = True
+            index = -1
+            while flag1:
+                try:
+                    if diagram[index] > 1:
+                        diagram[index] -= 1
+                        try:
+                            diagram[index+1] += 1
+                        except:
+                            diagram.append(1)
+                        c += 1
+                        print(diagram, c)
+                except:
+                    pass
+                index += 1
+                if len(diagram) > index:
+                    index = 0
         else:
             flag = False
 
     return c
 
-print(exp_sum(10))
+print(exp_sum(5))
 
+# переделать крч надо все =(
